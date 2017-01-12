b0118fa147e9d732415a0bcb06bfba852da1210e
aminmb <amin.mohammadbagheri@gmail.com>
2017-01-12 16:43:59 -0600

    Pusht the first version of the web App

--- HRVToolbox/server.R
+++ HRVToolbox/server.R
@@ -6,21 +6,110 @@
 # 
 #    http://shiny.rstudio.com/
 #
+options(shiny.maxRequestSize = 27*1024^2)
+options(rgl.useNULL=TRUE)
 
+
+library(rgl)
+library(rglwidget)
 library(shiny)
+library(shinyRGL)
+library(scatterplot3d)
+library(knitr)
+knit_hooks$set(webgl = hook_webgl)
+
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
+shinyServer(
+  function(input, output, session) {
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
     
+    output$rglplot <- renderRglwidget({
+      inFile <- input$physionetFile
+      if (is.null(inFile))
+        return(NULL)
+      DataRaw <- read.csv(inFile$datapath, header = input$header, sep = input$sep)
+      physionetSignalTime <- DataRaw [ , 1 ]
+      physionetSignalECG  <- DataRaw [ , 2 ]
+      physionetSignalEMG  <- DataRaw [ , 3 ]
+      physionetSignalFGSR <- DataRaw [ , 4 ]  # Foot GSR
+      physionetSignalHGSR <- DataRaw [ , 5 ]  # Hand GSR
+      
+      Data_Length <- length ( physionetSignalECG )
+      V <- physionetSignalECG
+      tau = input$physionetTau
+      
+      ###### Initializing Delayed Signals  ########
+      V_tau = rep (0, Data_Length)  # V (t-tau)
+      V_2_tau = rep (0, Data_Length)  # V (t-2*tau)
+      
+      ###### Calculating Delayed Signals  ########
+      for (i in 1: Data_Length) {
+        if ( i < tau ) {
+          V_tau [i] = 0
+          V_2_tau [i] = 0
+        } else if ( i >= tau && i <= 2*tau ) {
+          V_tau [i] <- V [ i - tau + 1 ]
+          V_2_tau [i] = 0
+        } else{
+          V_tau [i] <- V [ i - tau + 1 ]
+          V_2_tau [i] <- V [ i - 2*tau + 1 ]
+        }
+      }  
+      plot3d ( V, V_tau , V_2_tau )
+      #plot3d ( V, V_tau , V_2_tau, main = 'ECG tau = 15')
+      rglwidget()
+    })
+    
+    output$myPlot <- renderPlot ({
+      if (input$experiment == "PhysioNet Automobile Driver Stress"){
+        inFile <- input$physionetFile
+        if (is.null(inFile)) {
+          return(NULL)
+        }
+        DataRaw <- read.csv(inFile$datapath, header = input$header, sep = input$sep)
+        physionetSignalTime <- DataRaw [ , 1 ]
+        physionetSignalECG  <- DataRaw [ , 2 ]
+        physionetSignalEMG  <- DataRaw [ , 3 ]
+        physionetSignalFGSR <- DataRaw [ , 4 ]  
+        physionetSignalHGSR <- DataRaw [ , 5 ]
+        
+        if ( input$physionetPlotType == "TimeSeries") {
+          Data_Length <- length ( physionetSignalECG )
+          if (input$physionetSignal == "ECG") {
+            plot ( physionetSignalECG, type = "l", xlim = c (0, Data_Length))
+          } 
+          else if ( input$physionetSignal == "EMG") {
+            plot ( physionetSignalEMG, type = "l")
+          }
+          else if ( input$physionetSignal == "HGSR" ) {  
+            plot ( physionetSignalHGSR, type = "l")
+          }
+          else {
+            plot ( physionetSignalFGSR, type = "l")
+          }
+        }
+      }
+      else {
+        inFile <- input$empaticaFile
+        if (is.null(inFile)) {
+          return(NULL)
+        }
+        DataRaw <- read.csv ( inFile$datapath, header = input$header, sep = input$sep )
+        empaticaSignalTime <- DataRaw [ , 5 ]
+        empaticaSignalIBI  <- DataRaw [ , 2 ]
+        empaticaSignalEDA  <- DataRaw [ , 3 ]
+        empaticaSignalBVP  <- DataRaw [ , 4 ]
+        if ( input$empaticaSignal == "IBI" ){ 
+          plot ( empaticaSignalIBI, type = "l" )
+        }
+        else if ( input$empaticaSignal == "BVP" ) {
+          plot ( empaticaSignalEDA, type = "l" )
+        }
+        else if ( input$empaticaSignal == "HR" ) {
+          plot ( empaticaSignalBVP, type = "l" )
+        }
+      }
+    })
   })
-  
-})
--- HRVToolbox/ui.R
+++ HRVToolbox/ui.R
@@ -7,27 +7,88 @@
 #    http://shiny.rstudio.com/
 #
 
+options(rgl.useNULL=TRUE)
+library(rgl)
+library(rglwidget)
 library(shiny)
+library(shinyRGL)
+library(rgl)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Physiological Data Visualization"),
   
   # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+  sidebarLayout (
+    sidebarPanel (
+      selectInput ("experiment", "Please select the experiment",
+                   choices=c("PhysioNet Automobile Driver Stress","Empatica E4")),
+      conditionalPanel (condition = "input.experiment == 'Empatica E4'",
+                        radioButtons ("empaticaUpload", label = h3("Please select the database"),
+                                      choices = list ("Upload the .CSV file" = 1, "Using Libraries" = 2), selected = 1),
+                        conditionalPanel (condition = "input.empaticaUpload == 1",
+                                          fileInput('empaticaFile', 'Choose file to upload',
+                                                    accept = c('text/csv','text/comma-separated-values','text/tab-separated-values',
+                                                               'text/plain','.csv', '.tsv')
+                                          ),
+                                          checkboxInput('header', 'Header', TRUE),
+                                          radioButtons('sep', 'Separator', c(Comma=',',Semicolon=';', Tab='\t'),',')
+                        ),
+                        conditionalPanel (condition = "input.empaticaUpload == 2",
+                                          selectInput ("empaticaDate", "Please select the date",
+                                                       choices=c("June 30, 2016","July 22, 2016","July 29, 2016",
+                                                                 "August 19, 2016","October 12, 2016")
+                                          )
+                        ),
+                        selectInput ("empaticaSignal", "Please select the physiological measurement from Empatica E4",
+                                     choices=c("HR","BVP","IBI","EDA","Temp", "AccelX","AccelY","AccelZ")),
+                        selectInput ("empaticaPlotType", "Please select the type of plot",
+                                     choices=c("Time Series","3D Scatterplot")),
+                        conditionalPanel (condition = "input.empaticaPlotType == '3D Scatterplot'",
+                                          sliderInput ("empaticaTau", "Please select the delay time (tau):",
+                                                       min = 1, max = 120, value = 10, step = 1)
+                        )
+      ),
+      conditionalPanel (condition = "input.experiment == 'PhysioNet Automobile Driver Stress'",
+                        radioButtons ("physionetUpload", label = h3("Please select the database"),
+                                      choices = list ("Upload the .CSV file" = 1, "Using Libraries" = 2), selected = 1),
+                        conditionalPanel (condition = "input.physionetUpload == 1",
+                                          fileInput('physionetFile', 'Choose file to upload',
+                                                    accept = c('text/csv','text/comma-separated-values','text/tab-separated-values',
+                                                               'text/plain','.csv', '.tsv')
+                                          ),
+                                          checkboxInput('header', 'Header', TRUE),
+                                          radioButtons('sep', 'Separator', c(Comma=',',Semicolon=';', Tab='\t'),',')
+                        ),
+                        conditionalPanel (condition = "input.physionetUpload == 2",
+                                          selectInput ("physionetDate", "Please select the dataset",
+                                                       choices=c("driver01","driver02","driver03", "driver04","driver05")
+                                          )
+                        ),
+                        selectInput ("physionetSignal", "Please select the physiological measurement from Empatica E4",
+                                     choices=c("ECG","EMG","HGSR","FGSR")),
+                        selectInput ("physionetPlotType", "Please select the type of plot",
+                                     choices=c("Time Series","3D Scatterplot")),
+                        conditionalPanel (condition = "input.physionetPlotType == '3D Scatterplot'",
+                                          sliderInput ("physionetTau", "Please select the delay time (tau):",
+                                                       min = 1, max = 120, value = 10, step = 1)
+                        )
+      )
     ),
-    
-    # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+      tabsetPanel(
+        tabPanel("2D Plot", plotOutput("myPlot")),
+        tabPanel("3D Plot", rglwidgetOutput("rglplot"))
+      )
     )
+    
+    #fluidRow(
+    # splitLayout(cellWidths = c("50%", "50%"), rglwidgetOutput("rglplot"), plotOutput("myPlot"))
+    
+    
   )
-))
+)
+)
+
